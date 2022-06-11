2cd1c57746cb8e63784f9f71b9df86215f38b60c
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-11 00:30:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const  findMatching = function (test, drivers){
+    return test.filter(match => match.toLowerCase()=== drivers.toLowerCase())
+    }
+
+
+const fuzzyMatch = (test, drivers) => {
+   return test.filter((nam) => nam.slice(0, 2) == drivers.slice(0, 2))
+}
+const matchName = function (test, drivers){
+    return test.filter(record => record.name === drivers)
+    }
